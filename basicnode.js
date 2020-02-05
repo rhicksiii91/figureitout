14c07c4d4328d1be729c16e9c0cb56e30711ea0c
roberthicks <roberthicksiii@gmail.com>
2020-02-05 08:31:21 -0500

    updating the http message

--- basicnode.js
+++ basicnode.js
@@ -3,6 +3,7 @@ var datentime = require('./datentime');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(datentime.myDateTime());
+  res.write(datentime.myDateTime() +'<br/>');
+  res.write(req.url);
   res.end();
 }).listen(8081);
